f0240330cc108aabc08ab56936968b212e72155d
Shyam Bihari Swami <sbswami18@gmail.com>
2019-12-26 09:26:52 +0530

    Simple chat feature with socket.io and Bootstrap

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+users = [];
+connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log('Server Running...');
+app.get('/', function(req, res){
+  res.sendFile(__dirname+ '/index.html');
+});
+
+io.sockets.on('connection', function(socket){
+  connections.push(socket);
+  console.log('connected %s sockets connected!', connections.length);
+
+  // Disconnect
+  socket.on('disconnect', function(data){
+    // if (!socket.username) return;
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected: %s sockets connected', connections.length);
+  });
+
+  // Send Message
+  socket.on('Send Message', function(data){
+    console.log(data);
+    io.sockets.emit('New Message', {msg: data, username: socket.username});
+  });
+
+  // New User
+  socket.on('New User', function(data, callback){
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+  });
+
+  function updateUsernames(){
+    io.sockets.emit('Get Users', users);
+  }
+
+});
